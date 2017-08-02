514a79f2432e86dd6b825425d1315cab7bddf9f7
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2017-08-01 21:12:12 -0700

    More fixes of the incorrect ' characters

--- lr.py
+++ lr.py
@@ -40,14 +40,14 @@ logging.getLogger().setLevel(logging.DEBUG)
 train_data = np.random.uniform(0, 1, [100, 2])
 train_label = np.array([train_data[i][0] + 2 * train_data[i][1] for i in range(100)])
 batch_size = 1
-X = mxnet.sym.Variable(‘data’)
-Y = mxnet.symbol.Variable(‘lin_reg_label’)
+X = mxnet.sym.Variable('data')
+Y = mxnet.symbol.Variable('lin_reg_label')
 fully_connected_layer = mxnet.sym.FullyConnected(data=X, name='fc1', num_hidden = 1)
 lro = mxnet.sym.LinearRegressionOutput(data=fully_connected_layer, label=Y, name="lro")
 model = mxnet.mod.Module(
  symbol = lro ,
- data_names=[‘data’],
- label_names = [‘lin_reg_label’]# network structure
+ data_names=['data'],
+ label_names = ['lin_reg_label']# network structure
 )
 #Evaluation Data
 eval_data = np.array([[7,2],[6,10],[12,2]])
@@ -55,8 +55,8 @@ eval_label = np.array([11,26,16])
 train_iter = mxnet.io.NDArrayIter(train_data,train_label, batch_size, shuffle=True,label_name=’lin_reg_label’)
 eval_iter = mxnet.io.NDArrayIter(eval_data, eval_label, batch_size, shuffle=False)
 model.fit(train_iter, eval_iter,
- optimizer_params={‘learning_rate’:0.005, ‘momentum’: 0.9},
+ optimizer_params={'learning_rate':0.005, 'momentum': 0.9},
  num_epoch=50,
- eval_metric=’mse’,
+ eval_metric='mse',
  batch_end_callback = mxnet.callback.Speedometer(batch_size, 2),
  kvstore=kv_store)
